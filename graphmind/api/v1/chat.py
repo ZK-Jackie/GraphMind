f3535f56d1307f9f6adcb07319de6a1468eb3c0d
Jackie <44011334mm@stu.gdou.edu.cn>
2024-09-27 21:59:59 +0800

    feat: http api support

---
+++ graphmind/api/v1/chat.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
+
+from graphmind.service.base import ChatMessage
+from graphmind.service.chain import rag_chain
+
+api_chat = APIRouter()
+
+@api_chat.post("/test")
+async def test():
+    return {"code": "200", "msg": "ok", "data": "hello, world!"}
+
+@api_chat.post("/invoke")
+async def post_chat(chat_input: ChatMessage) -> ChatMessage:
+    return rag_chain.invoke(chat_input)
+
+@api_chat.post("/stream")
+async def chat(chat_input: ChatMessage):
+    async def event_generator():
+        async for chunk in rag_chain.stream(chat_input):
+            yield chunk
+
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+
